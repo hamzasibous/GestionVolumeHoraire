b588e50a5757f2a8c8bd710b49c996f19fbdaf6e
prince <hay.benyoussef@edu.umi.ac.ma>
2026-04-25 18:28:02 +0100

    added models,polished older ones

--- core/models.py
+++ core/models.py
@@ -1,53 +1,84 @@
 from django.db import models
 
-# Create your models here.
+from users.models import Utilisateur
+
+# the core models of the application
+
+
 class Departement(models.Model):
     nom = models.CharField(max_length=100)
-    
-    def __str__(self):
-        return self.nom
-    
 
-class Utilisateur(models.Model):
-    id=models.AutoField(primary_key=True)
-    nom = models.CharField(max_length=100)
-    prenom = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    tel = models.CharField(max_length=20, blank=True, null=True)
-    motDePasse = models.CharField(max_length=128)
     def __str__(self):
-        return f"{self.prenom} {self.nom}"
-    def connecter(self, email, motDePasse):
-        return ""
+        return f"{self.nom}"
+
 
 class Local(models.Model):
-    id=models.AutoField(primary_key=True)
-    bloc=models.CharField(max_length=100)
-    numero=models.CharField(max_length=100)
-    capacite=models.IntegerField()
+    bloc = models.CharField(max_length=100)
+    numero = models.IntegerField()
+    capacite = models.IntegerField()
 
     def __str__(self):
-        return self.nom
-    
+        return f"{self.numero}.{self.numero}"
+
+
 class Filiere(models.Model):
-    id=models.AutoField(primary_key=True)
-    niveau=models.CharField(max_length=100)
-    nom=models.CharField(max_length=100)
-    departement=models.ForeignKey(Departement, on_delete=models.CASCADE)
-    
+    class niveaux(models.TextChoices):
+        COURS = "License"
+        TD = "Master"
+        TP = "Doctorat"
+
+    niveau = models.CharField(max_length=10, choices=niveaux.choices)
+    nom = models.CharField(max_length=100)
+    departement = models.ForeignKey(Departement, on_delete=models.CASCADE)
+
     def __str__(self):
-        return self.nom
+        return f"{self.nom}"
+
     def get_modules(self):
-        return Module.objects.filter(filiere=self)
+        return self.modules.all()
+
     def getDepartement(self):
         return self.departement
-    
-    
 
 
 class Module(models.Model):
-    id=models.AutoField(primary_key=True)
-    nom=models.CharField(max_length=100)
-    semestre=models.IntegerField()
+    nom = models.CharField(max_length=100)
+    semestre = models.IntegerField()
+    filiere = models.ForeignKey(
+        Filiere, on_delete=models.CASCADE, related_name="modules"
+    )
+
     def __str__(self):
-        return self.nom
+        return f"{self.nom}"
+
+
+class Enseignant(Utilisateur):
+    departement = models.ForeignKey(
+        Departement, on_delete=models.CASCADE, related_name="enseignants"
+    )
+    is_chef_departement = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return f"{self.departement} / Pr.{self.nom}"
+
+
+class Sceance(models.Model):
+
+    class Type(models.TextChoices):
+        COURS = "CM", "Cours"
+        TD = "TD", "TD"
+        TP = "TP", "TP"
+
+    type = models.CharField(max_length=50, choices=Type.choices)
+    duration = models.IntegerField()
+    nombre_sceances = models.IntegerField()
+    module = models.ForeignKey(
+        Module, on_delete=models.CASCADE, related_name="sceances"
+    )
+    local = models.ForeignKey(Local, on_delete=models.CASCADE, related_name="sceances")
+    enseignant = models.ForeignKey(
+        Enseignant, on_delete=models.CASCADE, related_name="sceances"
+    )
+
+    def __str__(self) -> str:
+        return f"{self.module}/{self.type}"
--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Utilisateur(models.Model):
+    nom = models.CharField(max_length=100)
+    prenom = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    tel = models.CharField(max_length=20, blank=True, null=True)
+    motDePasse = models.CharField(max_length=128)
+
+    def __str__(self):
+        return f"{self.prenom} {self.nom}"
+
+    def connecter(self, email, motDePasse):
+        return ""
